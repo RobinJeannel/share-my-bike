c8b142b7f5883933e933a0ae2667901e7c197355
Nordine Slimani <slimaninordine@gmail.com>
2014-08-19 11:09:59 +0200

    delete bike controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   resources :users
-  resources :bikes
-    resources :offers do
-      resources :review
-    end
+  resources :offers do
+    resources :review
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
