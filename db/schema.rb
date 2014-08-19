fd06adc7fffc5c90183da218b95af259697ee024
Juliette Sierra <sanson.juliette@gmail.com>
2014-08-19 11:15:38 +0200

    de la merde

--- db/schema.rb
+++ db/schema.rb
@@ -54,4 +54,13 @@ ActiveRecord::Schema.define(version: 20140818175744) do
 
   add_index "reviews", ["Offer_id"], name: "index_reviews_on_Offer_id", using: :btree
 
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
