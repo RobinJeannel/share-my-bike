13cbe30380ad051ba066e51e64d071a84cbdeb5a
RobinJeannel <rjeanneldethiersant@gmail.com>
2014-08-19 14:51:18 +0200

    upload img fixed

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,7 @@ Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:rails_env/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_credentials] = {
-  bucket: ENV['share-my-bike'],
-  access_key_id: ENV['AKIAJW3LBW2NWXQOAPUA'],
-  secret_access_key: ENV['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']
+  bucket: ENV['S3_BUCKET_NAME'],
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
 }
